bfa9f7c0dfc3baa7e3bfe513b29e3fc820060b77
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-02-01 12:52:31 +0100

    Server pdoo

--- index.js
+++ index.js
@@ -1,43 +1,11 @@
 //**************************************************************************
 
-// Config server
+// Start server
 
 //**************************************************************************
 
-const express = require('express');
-const cors = require('cors');
-const env  = require ('dotenv');
-const env_expand = require('dotenv-expand');
-
-const { dbConnection } = require('./database/config.js');
-
-env_expand( env.config() );
+const server = require('./server')
 
 //--------------------------------------------------------------------------
 
-// Create server
-const app = express();
-
-// Connect DB
-dbConnection();
-
-// CORS
-app.use( cors() );
-
-// Public directory
-app.use( express.static('public') );
-
-// Read body correctly
-app.use( express.json() );
-
-// Routes
-app.use( '/api/auth', require('./routes/auth.js') );
-
-//Listen requests
-app.listen( process.env.PORT, () => {
-    console.log('Server running on port', process.env.PORT);
-} );
-
-//--------------------------------------------------------------------------
-
- 
+server.start();
---
+++ server.js
@@ -0,0 +1,54 @@
+//**************************************************************************
+
+// Config server
+
+//**************************************************************************
+
+const express = require('express');
+const cors = require('cors');
+const env  = require ('dotenv');
+const env_expand = require('dotenv-expand');
+
+const { dbConnection } = require('./database/config.js');
+
+env_expand( env.config() );
+
+//--------------------------------------------------------------------------
+
+class Server {
+    
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        
+        this.setDatabase();
+        this.setMiddleware();
+        this.setRoutes();
+    }
+    
+    setDatabase() {
+        dbConnection();
+    }
+
+    setMiddleware() {
+        this.app.use( cors() );
+        this.app.use( express.static('public') );
+        this.app.use( express.json() );
+    }
+    
+    setRoutes() {
+        this.app.use( '/api/auth', require('./routes/auth.js') );
+    }
+
+    start() {
+        this.app.listen( this.port, () => {
+            console.log('Server running on port', this.port);
+        } );
+    }
+}
+
+//--------------------------------------------------------------------------
+
+ const server = new Server();
+
+ module.exports = server;
